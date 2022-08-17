0f213d745a89666dfefe6165979dfff26cfd4b52
Cristóbal Alcázar <alcazar.cristobal90@gmail.com>
2022-08-16 22:54:09 -0400

    Actualizar template, y modulos: dataset y model

--- demo/template/proj_code/dataset.py
+++ demo/template/proj_code/dataset.py
@@ -1,23 +1,29 @@
 """
     dataset.py: modulo para cargar y pre-procesar datos.
+    Devolver solo X e Y y mover el train_test_split...
 """
 
 import pandas as pd
-from sklearn.model_selection import train_test_split
 
 DATA_SOURCE = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+LOCAL_PATH = "./data/iris.data"
 COLUMN_NAMES = ["sepal_length", "sepal_width", "petal_length", "petal_width", "species"]
-TEST_SIZE=0.33
-RND_STATE=42
 
 
-def load_dataset(URL=DATA_SOURCE):
-    """Descarga desde el repositorio UCI el dataset iris y retorna una tupla con los siguientes dataframes:
-     X_train, X_test, y_train, y_test."""
+def load_iris_web(URL=DATA_SOURCE):
+    """Descarga desde el repositorio UCI el dataset iris y retorna un diccionario con el 'dataset', una tupla que
+    contiene X (input) e y (output), además de las 'etiquetas'."""
     print("Datos cargados con éxito")
     df = pd.read_csv(URL, names=COLUMN_NAMES, header=None)
     X, y = df.iloc[:, 0:4], df.iloc[:, [4]]
     labels = {key: id for key, id in zip(y.species.unique(), [x for x in range(3)])}
     y = y.replace({"species": labels}).squeeze()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, random_state=RND_STATE)
-    return X_train, X_test, y_train, y_test, labels
+    return {"dataset": (X, y),
+            "labels": labels}
+
+
+def load_iris_local(PATH=LOCAL_PATH):
+    """Cargar el dataset desde una ruta en disco local. Específicamente un archivo guardado en la carpeta ./data"""
+    print("Datos cargados con éxito")
+    return pd.read_csv(PATH, names=COLUMN_NAMES, header=None)
+
--- demo/template/proj_code/model.py
+++ demo/template/proj_code/model.py
@@ -2,9 +2,20 @@
     model.py: modulo donde se especifica el modelo
 """
 from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
 
 RND_STATE=42
 
-modelo_clasificacion = LogisticRegression(random_state=RND_STATE)
 
+def reg_logistica(X, y):
+    """Documentación de este modelo: """
+    clf = LogisticRegression(random_state=RND_STATE)
+    return clf.fit(X, y)
+
+
+def gaussian_naive_bayes(X, y):
+    """Documentación de este modelo: https://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.GaussianNB
+    .html """
+    clf = GaussianNB()
+    return clf.fit(X, y)
 
